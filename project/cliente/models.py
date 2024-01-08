db61fc4a387332471f3a91f30fd52376072c5ec9
sebastianmorenocba <ab.sebastianmoreno@gmail.com>
2024-01-08 03:16:33 -0300

    Fin clase 10

--- project/cliente/models.py
+++ project/cliente/models.py
@@ -6,12 +6,14 @@ class Pais(models.Model):
     nombre = models.CharField(max_length=100)
     def __str__(self) -> str:
         return self.nombre
+    class Meta: 
+        verbose_name_plural = "Países"
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     nacimiento = models.DateField(null=True, blank=True)
-    pais_origen_id = models.ForeignKey(Pais, null=True, blank=True, on_delete=models.SET_NULL)    
+    pais_origen_id = models.ForeignKey(Pais, null=True, blank=True, on_delete=models.SET_NULL, verbose_name="pais de origen")    
 
     def __str__(self) -> str:
         return f"{self.apellido.upper()}, {self.nombre}"
