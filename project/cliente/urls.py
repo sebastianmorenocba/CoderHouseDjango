1f93f0c379db4a5f3713f0fff8f8c4810a6b3588
sebastianmorenocba <ab.sebastianmoreno@gmail.com>
2024-01-08 03:19:33 -0300

    Fin de clase 10

--- project/cliente/urls.py
+++ project/cliente/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from.views import index
 
+app_name = "cliente"
+
 urlpatterns = [
-    path('', index),
+    path('', index, name = "index"),
 ]
--- project/core/urls.py
+++ project/core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from.views import index
 
+app_name = "core"
+
 urlpatterns = [
-    path('', index),
+    path('', index, name = "index"),
 ]
